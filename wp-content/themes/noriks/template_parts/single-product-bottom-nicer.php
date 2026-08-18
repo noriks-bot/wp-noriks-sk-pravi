f388637834c36eb135d57c2207e6cd016be9dddc
Noriks (via Claude) <info@noriks.com>
2026-08-18 15:05:30 +0200

    Recenzije na stranicama majica: bazen proizvoda samo iz kategorije majica (prije su zavrsavale pod orto proizvodima)

--- wp-content/themes/noriks/template_parts/single-product-bottom-nicer.php
+++ wp-content/themes/noriks/template_parts/single-product-bottom-nicer.php
@@ -806,7 +806,7 @@ endif;
    * Build/caches a pool of products: [['title'=>..., 'url'=>...], ...]
    */
   function get_wc_product_pool(
-      $transient_key = 'reviews_product_pool_cache_v2',
+      $transient_key = 'reviews_product_pool_cache_v3',
       $ttl = 12 * HOUR_IN_SECONDS
   ) {
       if ( ! function_exists( 'wc_get_products' ) ) {
@@ -855,14 +855,10 @@ endif;
       } elseif ( $is_bokserice ) {
           $args['category'] = [ 'boxerky' ];
       } else {
-          $args['tax_query'] = [
-              [
-                  'taxonomy' => 'product_cat',
-                  'field'    => 'slug',
-                  'terms'    => [ 'boxerky' ],
-                  'operator' => 'NOT IN',
-              ],
-          ];
+          // Stranice majica: bazen SAMO iz kategorije majica (s podkategorijama).
+          // Prije je uzimao sve osim bokserica, pa su recenzije o majicama
+          // zavrsavale pod orto proizvodima (Cloth XXL, Cool Curl…).
+          $args['category'] = [ 'tricka' ];
       }
 
       $ids = wc_get_products( $args );
